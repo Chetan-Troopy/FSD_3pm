6993156f5e0a39c99c2eb7125e77468e5a6716b8
Chetan-Troopy <chetan.sharma@troopystack.com>
2025-06-04 15:50:48 +0530

    Lecture Data

--- React/firebasereactapp/src/App.jsx
+++ React/firebasereactapp/src/App.jsx
@@ -3,11 +3,16 @@ import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import Logout from "./components/Logout";
+import GoogleLogin from "./components/GoogleLogin";
 
 function App() {
   return (
     <>
       <div className="container">
+        <div className="col py-5">
+          <h1>firebase Google Auth</h1>
+          <GoogleLogin/>
+        </div>
         <div className="col py-5">
           <Profile />
         </div>
--- React/firebasereactapp/src/firebase/firebaseConfig.js
+++ React/firebasereactapp/src/firebase/firebaseConfig.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCuTzKISOzICnc5fXdR7R2PWPuaqsQNLlA",
-  authDomain: "webmania-2a038.firebaseapp.com",
-  projectId: "webmania-2a038",
-  storageBucket: "webmania-2a038.firebasestorage.app",
-  messagingSenderId: "68691570221",
-  appId: "1:68691570221:web:1254ddcf1bddf3c70592a4",
-  measurementId: "G-JQ9ZSKFZ82"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider();
